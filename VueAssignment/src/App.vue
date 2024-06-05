0094b83059e7866b328f28b78430c93f538f3fd2
Jan1Verse1 <oluwaseyiadedotun@gmail.com>
2024-06-05 03:16:57 +0100

    Home page progress

--- VueAssignment/src/App.vue
+++ VueAssignment/src/App.vue
@@ -1,27 +1,16 @@
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-</script>
 
 <template>
-  <div> 
-    <H2>HELLO WORLD</H2>
+ 
+  
+  <div class="w-full">
+    <router-view></router-view>
   </div>
-  <router-view></router-view>
+
   
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+<script setup>
+</script>
+
--- VueAssignment/src/views/Home.vue
+++ VueAssignment/src/views/Home.vue
@@ -1,3 +1,183 @@
 <template>
-    <H1>Home</H1>
-</template>
+    <div>
+      <!-- Header -->
+      <div class="bg-cyan-900">
+        <nav class="p-4 flex justify-between items-center">
+          <router-link
+            class="flex row items-center shadow hover:shadow-lg active:shadow-xl p-4 rounded-xl transition duration-150 ease-in-out cursor-pointer"
+            to="/"
+          >
+            <div v-if="profileData">
+              <img
+                :src="profileData.avatar_url"
+                alt="My GitHub Avatar"
+                class="h-12 w-12 rounded-full mr-2"
+              />
+            </div>
+            <h4 class="text-white text-lg">Jan1Verse1</h4>
+          </router-link>
+          <!-- <button
+            @click="openModal"
+            class="text-white text-lg duration-150 ease-in-out cursor-pointer"
+          >
+            Add A Repository
+          </button> -->
+        </nav>
+        <MyModal v-if="showModal" @close="closeModal" />
+      </div>
+  
+      <!-- Head -->
+      <div class="p-6">
+        <img src="" alt="" />
+        <div class="typing-demo">
+          <p class="text-4xl mt-10 mb-10 md:text-6xl lg:text-8xl">{{ text1 }}</p>
+          <p class="text-2xl">
+            <span
+              class="inline-block text-md md:text-2xl lg:text-3xl"
+              v-html="text2"
+            />
+          </p>
+          <div v-if="typingComplete" class="mt-10">
+            <a
+              class="t-[200px] hover:cursor-pointer rounded-xl border-2 border-cyan-900 p-2 hover:bg-cyan-900 hover:text-white hover:border-2 text-2xl"
+              href="https://github.com/Jan1Verse1"
+              target="_blank"
+            >
+              My Résumé
+            </a>
+          </div>
+        </div>
+      </div>
+  
+      <!-- Repos -->
+      <div class="width-auto p-10 max-w-6xl flex-col justify-between items-center">
+        <h3 class="text-xl font-semibold mb-4">My Repositories</h3>
+        <div class="mb-4 justify-end mt-6 sm:flex-col">
+          <input
+            type="text"
+            placeholder="Search repositories..."
+            v-model="searchQuery"
+            class="px-4 py-2 border border-gray-300 rounded-md mr-2"
+          />
+          <select
+            v-model="filterLanguage"
+            class="px-4 py-2 border border-gray-300 rounded-md"
+          >
+            <option value="">All Languages</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="CSS">CSS</option>
+            <option value="HTML">HTML</option>
+            <option value="Shell">Shell</option>
+          </select>
+        </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          <RepoCard
+            v-for="repo in paginatedRepos"
+            :key="repo.id"
+            :name="repo.name"
+            :visibility="repo.visibility"
+            :language="repo.language"
+            :updatedAt="repo.updated_at"
+            :htmlUrl="repo.html_url"
+          />
+        </div>
+        <!-- <div class="flex justify-end mt-6">
+          <Pagination
+            :currentPage="currentPage"
+            :totalCount="filteredRepos.length"
+            :pageSize="repoSize"
+            @pageChange="handlePageChange"
+          />
+        </div> -->
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+//   import MyModal from './modals/MyModal.vue';
+//   import Pagination from "../components/Pagination.vue";
+//   import RepoCard from "../components/RepoCard.vue";
+  
+  export default {
+    name: "Home",
+    components: {
+    //   MyModal,
+    //   Pagination,
+    //   RepoCard
+    },
+    data() {
+      return {
+        profileData: null,
+        showModal: false,
+        text1: "Hello...",
+        text2: "",
+        typingComplete: false,
+        fullText: `I am <span style="color:#2b7bd7;"> Oluwaseyi Adedotun </span>, a <span class="blue-text"> Frontend Developer </span> with over 1 year experience proficiently building software with the following frontend technologies, <span class="blue-text">Javascript, React.js, Redux, Bootstrap, Tailwind CSS and others.</span> I believe in the principle of constant improvement and I am always trying to upskill by learning new technologies to add to my stack!`,
+        repos: [],
+        searchQuery: "",
+        filterLanguage: "",
+        currentPage: 1,
+        repoSize: 12,
+        GITHUB_TOKEN: import.meta.env.VITE_GITHUB_PERSONAL_TOKEN
+      };
+    },
+    created() {
+      this.fetchGitHubProfile();
+      this.typeText();
+      this.fetchGitHubRepos();
+    },
+    methods: {
+      async fetchGitHubProfile() {
+        try {
+          const response = await axios.get('https://api.github.com/users/Jan1Verse1');
+          this.profileData = response.data;
+        } catch (error) {
+          console.error('Error fetching GitHub profile:', error);
+        }
+      },
+      openModal() {
+        this.showModal = true;
+      },
+      closeModal() {
+        this.showModal = false;
+      },
+      typeText() {
+        let currentIndex = 0;
+        const typingInterval = setInterval(() => {
+          if (currentIndex <= this.fullText.length) {
+            this.text2 = this.fullText.substring(0, currentIndex);
+            currentIndex++;
+          } else {
+            clearInterval(typingInterval);
+            this.typingComplete = true;
+          }
+        }, 25);
+      },
+      async fetchGitHubRepos() {
+        try {
+          const config = {
+            headers: {
+              Authorization: `Bearer ${this.GITHUB_TOKEN}`
+            }
+          };
+          const response = await axios.get(
+            `https://api.github.com/users/Jan1Verse1/repos`,
+            config
+          );
+          this.repos = response.data;
+        } catch (error) {
+          console.error("Error fetching GitHub repos:", error);
+        }
+      },
+      handlePageChange(page) {
+        this.currentPage = page;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any scoped CSS here */
+  </style>
+  
